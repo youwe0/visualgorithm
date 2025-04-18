ea574dd7ab34273abd0bb23c9c02a00d4c2a1ba4
youwe0 <yuvrajaingh1508@gmail.com>
2025-04-19 05:13:07 +0530

    changed name

--- src/App.jsx
+++ src/App.jsx
@@ -5,20 +5,20 @@ import SearchVisualizer from "./components/SearchVisualizer";
 import PathfindingVisualizer from "./components/PathfindingVisualizer";
 import Footer from "./components/Footer";
 import GameOfLife from "./components/Gameoflife";
-// import Datashow from "./components/Datashow";
+
 
 const App = () => {
   return (
     
       <div className="min-h-screen flex flex-col">
         <div className="flex-grow">
-          {/* <Datashow /> */}
+    
           <Routes>
             {/* Define the routes here */}
             <Route path="/sorting" element={<SortingVisualizer />} />
             <Route path="/pathfinding" element={<PathfindingVisualizer />} />
             <Route path="/search" element={<SearchVisualizer />} />
-            <Route path="/gameoflife" element={<GameOfLife />} />
+            <Route path="/Gameoflife" element={<GameOfLife/>} />
 
             {/* Default route, in case no match */}
             <Route path="*" element={<PathfindingVisualizer />} />
