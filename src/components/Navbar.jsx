4e5d9f3ed2c5da810840c6c428b77c8cd322ec88
youwe0 <yuvrajaingh1508@gmail.com>
2025-04-30 15:18:53 +0530

    complt prjct

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,11 +4,11 @@ const Navbar = () => {
     return (
         <div className='bg-red-200 flex items-center justify-between px-4'>
             <div className="flex items-center gap-0.5 ">
-                <img src="/public/icon only.png" alt="" className='h-20' />
-                <img src="/public/name only.png" alt="" className='h-16' />
+                <img src="/src/assets/icon only.png" alt="" className='h-20' />
+                <img src="/src/assets/name only.png" alt="" className='h-16' />
             </div>
             <div className=''>
-                <a href="https://github.com/youwe0/visualgorithm"><img src="/public/github-line.svg" alt="" className='h-10' /></a>
+                <a href="https://github.com/youwe0/visualgorithm"><img src="/src/assets/github-line.svg" alt="" className='h-10' /></a>
             </div>
         </div>
     )
